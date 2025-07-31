796fdd047af1f05ef441db039c541c6af3334607
Antonio Sachinski <as.sachinski@gmail.com>
2025-07-31 09:45:13 -0300

    docs: breve explicação sobre o módulo

---
+++ protocolo HTTP.js
@@ -0,0 +1,4 @@
+// módulo do node que permite a comunicação entre cliente e servidor
+// pode ser usado para o desenvolvimento backend, porém esse módulo é bem limitado
+// uma solução seria o uso de frameworks como express
+
