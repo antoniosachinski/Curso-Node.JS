b820271ab2ff443aea7de3c61ab48344dc8b22cf
Antonio Sachinski <as.sachinski@gmail.com>
2025-08-05 16:21:15 -0300

    feat: adicionando escrita de parâmetros na tela

--- parâmetros/index.js
+++ parâmetros/index.js
@@ -4,5 +4,8 @@ var app = express();
 app.get("/", function(req, res){
     res.send("a");
 })
+app.get("/ola/:nome/:cargo", function(req, res){
+    res.send("Olá " + req.params.nome + ", seu cargo é " + req.params.cargo);
+})
 
 app.listen(8081);
