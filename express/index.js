27bddd4d0d6c0e4deb40942df1db531f8e6f1eff
Antonio Sachinski <as.sachinski@gmail.com>
2025-08-06 16:38:00 -0300

    feat: informativo de erro/ sucesso

--- express/index.js
+++ express/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 app = express();
 
-app.listen(8081); // criou um servidor com apenas 
+app.listen(8081, function(erro){
+    if (erro){
+        console.log("Ocorreu um erro!")
+    }else{
+        console.log("Servidor rodando!")
+    }
+}); // criou um servidor com apenas 
