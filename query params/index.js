23433e90bbf5532fe15c0d5e55519d7e4e009dc6
Antonio Sachinski <as.sachinski@gmail.com>
2025-08-07 11:25:00 -0300

    Update index.js

--- query params/index.js
+++ query params/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+// query params são parametros opicionais na sua rota, que o user pode acessar ou não
 app.listen(8081, function(erro){
     if(erro){
         console.log("Ocorreu um erro!")
